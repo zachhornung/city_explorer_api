6526578eb073d7795d3e7662c66a9c9792a34053
Zach Hornung <Zachhornung@comcast.net>
2021-02-22 15:55:18 -0800

    added more server files, /location now works

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const PORT = process.env.PORT || 3030;
 
 //================ Routes =======================
 
-
+app.get('/location', handleGetLocation);
+function handleGetLocation(req, res){
+  const dataFromTheFile = require('./data/location.json');
+  const output = new Location(dataFromTheFile, req.query.city);
+  res.send(output);
+}
+function Location(dataFromTheFile, cityName){
+  this.search_query = cityName;
+  this.formatted_query = dataFromTheFile[0].display_name;
+  this.latitude = dataFromTheFile[0].lat;
+  this.longitude = dataFromTheFile[0].lon;
+}
 //=================== Initialization =================
 app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`));
