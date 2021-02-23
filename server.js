f2246210c74e9b8ca19ea6d36c53a80b8b1c4a35
Zach Hornung <Zachhornung@comcast.net>
2021-02-22 16:36:52 -0800

    added weather path

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 3030;
 //================ Routes =======================
 
 app.get('/location', handleGetLocation);
+app.get('/weather', handleGetWeather);
+
+
 function handleGetLocation(req, res){
   const dataFromTheFile = require('./data/location.json');
   const output = new Location(dataFromTheFile, req.query.city);
@@ -23,5 +26,23 @@ function Location(dataFromTheFile, cityName){
   this.latitude = dataFromTheFile[0].lat;
   this.longitude = dataFromTheFile[0].lon;
 }
+
+function handleGetWeather(req, res){
+  const cityName = req.query.city;
+  console.log(cityName);
+  const dataFromTheFile = require('./data/weather.json');
+  const output = [];
+  dataFromTheFile.data.forEach(day => {
+    output.push(new Weather(day));
+  });
+  res.send(output);
+}
+
+function Weather(data){
+  this.forecast = data.weather.description;
+  this.time = data.valid_date;
+}
+
+
 //=================== Initialization =================
 app.listen(PORT, () => console.log(`app is up on port http://localhost:${PORT}`));
